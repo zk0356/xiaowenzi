fd8e68bb8b70851ae6efdb349f1c843207063f44
zak <edward.zhaoke@gmail.com>
2017-10-29 15:47:10 +0800

    增加.gitignore文件

--- src/main/java/com/xxx/jdk8/collection/stream/StreamDemo.java
+++ src/main/java/com/xxx/jdk8/collection/stream/StreamDemo.java
@@ -14,19 +14,19 @@ public class StreamDemo {
     public static void main(String[] args) {
         List<Nominator> nominatorList = new ArrayList<>();
         nominatorList.add(new Nominator("zak",3));
-        nominatorList.add(new Nominator("judd",2));
-        nominatorList.add(new Nominator("zak",3));
+        nominatorList.add(new Nominator("judd",4));
+        nominatorList.add(new Nominator("bond",1));
         nominatorList.add(new Nominator("sigmond",2));
         nominatorList.add(new Nominator("wason",5));
         nominatorList.stream().sorted().distinct().collect(Collectors.toList()).forEach(System.out::println);
 
-        nominatorList
-                .stream()
-                .collect(Collectors.groupingBy(Nominator::getVote))
-                .entrySet()
-                .stream()
-                .filter(integerListEntry -> integerListEntry.getValue().size() > 1)
-                .map(integerListEntry -> integerListEntry.getValue().stream().map(Nominator::getName).collect(Collectors.toList()))
-                .forEach(System.out::println);
+//        nominatorList
+//                .stream()
+//                .collect(Collectors.groupingBy(Nominator::getVote))
+//                .entrySet()
+//                .stream()
+//                .filter(integerListEntry -> integerListEntry.getValue().size() > 1)
+//                .map(integerListEntry -> integerListEntry.getValue().stream().map(Nominator::getName).collect(Collectors.toList()))
+//                .forEach(System.out::println);
     }
 }
