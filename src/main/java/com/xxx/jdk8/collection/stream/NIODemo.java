ca388d6695889cd173df6e5279addeddb5e10345
zak <edward.zhaoke@gmail.com>
2018-02-11 17:32:45 +0800

    流与NIO

---
+++ src/main/java/com/xxx/jdk8/collection/stream/NIODemo.java
@@ -0,0 +1,23 @@
+package com.xxx.jdk8.collection.stream;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * Created by xiaowenzi on 2018/2/9.
+ */
+public class NIODemo {
+    public static void main(String[] args) {
+        String fileName = "README.txt";
+        final Path path = new File(fileName).toPath();
+        try( Stream< String > lines = Files.lines( path, StandardCharsets.UTF_8 ) ) {
+            lines.onClose( () -> System.out.println("Done!") ).forEach( System.out::println );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
